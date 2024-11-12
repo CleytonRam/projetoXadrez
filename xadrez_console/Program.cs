70c300c3bffd26ccb8954cad9cbeab495f29c3aa
CleytonRam <cleytonglauco@gmail.com>
2024-11-11 23:57:27 -0300

    feat: create inital position

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tabuleiro;
+
+
+namespace xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+
+            Position p;
+
+            p = new Position(2, 4);
+        }
+    }
+}
