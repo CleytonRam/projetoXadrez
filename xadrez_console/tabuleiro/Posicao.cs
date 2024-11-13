a87ce1489b5a6e11a5705948dd571c4fe75f3681
CleytonRam <cleytonglauco@gmail.com>
2024-11-13 12:53:12 -0300

    feat: added tray to where the piece can move, added difrent moves to the pieces that already existed

--- xadrez_console/tabuleiro/Posicao.cs
+++ xadrez_console/tabuleiro/Posicao.cs
@@ -10,7 +10,10 @@ namespace tabuleiro {
             this.linha = linha;
             this.coluna = coluna;
         }
-
+        public void definirValores (int linha, int coluna) {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
         public override string ToString() {
             return linha
                 + ", "
